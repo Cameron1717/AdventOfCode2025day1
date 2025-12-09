6fbaa0d59dfdbbf08b282a8d7b99d4c5b8677943
Anise <cameron.casebolt17@gmail.com>
2025-12-09 01:16:50 -0800

    added part2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,19 +3,19 @@ use std::io::{self, BufRead};
 use std::path::Path;
 fn main() {
     println!("Advent of code Day 1 script");
-    //    let mut combination = Vec::new();
     let mut position = 50;
     let mut final_combo = 0;
+    let mut zero_counter = 0;
     if let Ok(lines) = read_lines("./input.txt") {
         for line in lines.map_while(Result::ok) {
-            //combination.push(line);
-            position = ouroboros(&line, position);
+            (position, zero_counter) = ouroboros(&line, position, zero_counter);
             if position == 0 {
                 final_combo += 1;
             }
         }
-    }
+    };
     println!("final combination: {}", final_combo);
+    println!("Part 2 answer {}", zero_counter)
 }
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where
@@ -24,19 +24,30 @@ where
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
-fn ouroboros(input: &String, mut position: i32) -> i32 {
+
+fn ouroboros(input: &String, mut position: i32, mut zero_counter: i32) -> (i32, i32) {
     let (direction, rotations) = input.split_at(1);
-    let i_rotations = rotations.parse::<i32>().unwrap_or_default();
-    if direction == "L" {
-        position -= i_rotations;
-    } else {
-        position += i_rotations;
+    let val = rotations.parse::<i32>().unwrap_or_default();
+
+    let full = val / 100;
+    let partial = val % 100;
+    zero_counter += full;
+
+    let delta = if direction == "L" { -partial } else { partial };
+    let next_position = position + delta;
+
+    if position != 0 {
+        if direction == "L" && next_position <= 0 {
+            zero_counter += 1;
+        } else if direction == "R" && next_position >= 100 {
+            zero_counter += 1;
+        }
     }
-    while position < 0 {
+
+    position = next_position % 100;
+    if position < 0 {
         position += 100;
     }
-    while position > 99 {
-        position -= 100
-    }
-    position
+
+    (position, zero_counter)
 }
